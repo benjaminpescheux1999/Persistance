fbba61b44afba4c1f1c30694d17014002fdcc481
benjaminpescheux1999 <pescheux.benjamin1702@gmail.com>
2021-09-21 12:08:17 +0200

    class Vendeur

---
+++ Vendeur.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance
+{
+    class Vendeur
+    {
+        public string nom { get; set; }
+        public string prenom { get; set; }
+        public string fonction { get; set; }
+        public string nomdumanager { get; set; }
+        public Vendeur()
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.fonction = fonction;
+            this.nomdumanager = nomdumanager;
+        }
+    }
+}
